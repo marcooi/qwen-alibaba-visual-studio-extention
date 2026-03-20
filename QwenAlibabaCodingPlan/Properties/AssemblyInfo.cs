2612e923745a176a6b342f7e001c48cfec53f7b6
marcooi <qomarudin@gmail.com>
2026-03-20 22:38:03 +0700

    fix buid error

---
+++ QwenAlibabaCodingPlan/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QwenAlibabaCodingPlan")]
+[assembly: AssemblyDescription("AI-powered coding assistant extension for Visual Studio 2022")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alibaba")]
+[assembly: AssemblyProduct("Qwen Alibaba Coding Plan")]
+[assembly: AssemblyCopyright("Copyright © Alibaba 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
